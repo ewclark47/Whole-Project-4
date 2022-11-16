34ac50d948e45563a4f06654522f11bf2bdd47d6
ewclark47 <93678940+ewclark47@users.noreply.github.com>
2022-11-16 15:26:23 -0500

    got categories in useable format, next work on geting them to show up in the dropdown. Deleted the intellij files since all will be in android app

--- Project4Task1_MobileApp/app/src/main/java/edu/cmu/project4task1_mobileapp/MainActivity.java
+++ Project4Task1_MobileApp/app/src/main/java/edu/cmu/project4task1_mobileapp/MainActivity.java
@@ -2,13 +2,42 @@ package edu.cmu.project4task1_mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainActivity ma = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainActivity me = this;
+
+        Button submitButton = (Button)findViewById(R.id.buttonSubmit);
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View viewParam){
+                //insert the stuff here to show the actual questions
+                //getQuestions gq = new getQuestions();
+            }
+        });
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinnerCategory);
+
     }
+
 }
---
+++ Project4Task1_MobileApp/app/src/main/java/edu/cmu/project4task1_mobileapp/getCategories.java
@@ -0,0 +1,60 @@
+package edu.cmu.project4task1_mobileapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class getCategories {
+    MainActivity ma = null;
+    TreeMap<String, Integer> catMap = new TreeMap<>();
+
+    public String getAPICategories() throws IOException {
+        URL url = new URL("https://opentdb.com/api_category.php");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        int responseCode = urlConnection.getResponseCode();
+        System.out.println("GET Response code :: " + responseCode);
+        if (responseCode == urlConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            String inLine;
+            while ((inLine = in.readLine()) != null) {
+                response.append(inLine);
+            }
+            in.close();
+            System.out.println(response);
+            response = response.replace( 0,22, "");
+            response = response.replace( response.length()-2,response.length(), "" );
+            return response.toString();
+        } else {
+            System.out.println("Error found");
+            return "Error";
+        }
+    }
+
+    public TreeMap<String, Integer> mapCategories(String str){
+        String[] splitString = str.split( "}," );
+        String longId;
+        int id=0;
+        String name;
+        String trash;
+        for(String line: splitString){
+            line = line.replace( "{","" ).replace( "}","" );
+            longId = line.split(",")[0];
+            name = line.split(",")[1];
+            id = Integer.parseInt( longId.split( ":" )[1] );
+            name = name.split( ":{1}" )[1];
+            catMap.put(name.replace( "\"","" ),id);
+            String[] parts = line.split( "," );
+        }
+        return catMap;
+    }
+
+    /*public static void mapToXML(HashMap<String, Integer> map){
+
+    }*/
+}
